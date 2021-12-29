f86245af8c73c47fe5a21187ada4c9e553d88111
Omer Gery <68545675+OmerGery@users.noreply.github.com>
2021-12-29 21:05:11 +0200

    wip

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} id='mainLogo'   />
+        <p>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
