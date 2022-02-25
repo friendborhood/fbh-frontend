a0c00f1a46b91981adb703ae36b52221365533c3
Omer Gery <omergery1996@gmail.com>
2022-02-25 15:16:52 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.jpg';
 import './App.css';
-
+import axios from 'axios'
+const makeRequest = async() => {
+  const {data} = await axios.get('http://localhost:3000/users/4');
+  alert(JSON.stringify(data));
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} id='mainLogo'   />
+        <button onClick={makeRequest}>Click!</button>
         <p>
         </p>
       </header>
