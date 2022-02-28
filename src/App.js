65482125bc2bfa1835bd25a243c8edc569fe2412
Omer Gery <omergery1996@gmail.com>
2022-02-28 23:25:28 +0200

    move main folder to be in root

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.jpg';
+import logo from './logo.svg';
 import './App.css';
+import BoxInput from './components/BoxInput';
+import Form from './components/Form'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} id='mainLogo'   />
-        <p>
-        </p>
-      </header>
+    <>
+    <div className='flex flex-col items-center'>
+      <img src="./photo/logo.png" width="600px"/>
+      <Form/>
     </div>
+    </>
   );
 }
 
