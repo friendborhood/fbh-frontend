a2c2d451c7e012c0c751b25af4b36ec8ebe1cf79
Omer Arzi <arziomer@gmail.com>
2022-03-10 20:33:51 +0200

    Route foundations added and states for signup added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,44 @@
 import BoxInput from "./BoxInput";
+import { useState } from "react";
+function Form(props) {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
 
-function Form(props){
-    return(
-        <div id='form' className="basis-full border-8 border-green-900 rounded-lg border-double">
-        <BoxInput label="First Name" id="fname" margin="mt-6"/>
-        <BoxInput label="Last Name" id="lname"/>
-        <BoxInput label="Username" id="username"/>
-        <BoxInput label="password" id="password"/>
-        <BoxInput label="Email Address" id="email" margin="mb-6"/>
-        </div>
-    );
+  return (
+    <form
+      id="form"
+      className="basis-full border-8 border-green-900 rounded-lg border-double"
+    >
+      <BoxInput
+        label="First Name"
+        id="fname"
+        state={fname}
+        setState={setFname}
+        margin="mt-6"
+      />
+      <BoxInput
+        label="Last Name"
+        id="lname"
+        state={lname}
+        setState={setLname}
+      />
+      <BoxInput
+        label="Username"
+        id="username"
+        state={username}
+        setState={setUsername}
+      />
+      <BoxInput
+        label="Email Address"
+        id="email"
+        state={email}
+        setState={setEmail}
+        margin="mb-6"
+      />
+    </form>
+  );
 }
 
 export default Form;
