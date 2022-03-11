dc1d294261a75f5d19b012a936e20b338b1695c1
Omer Gery <omergery1996@gmail.com>
2022-03-11 18:27:37 +0200

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,20 @@
-test('place holder', () => {
-  expect(true).toBeTruthy();
+import Puppeteer from 'puppeteer';
+
+jest.setTimeout(1000 * 60);
+let browser;
+let page;
+const LOCAL_URL = 'http://localhost:3000/';
+beforeAll(async () => {
+  browser = await Puppeteer.launch({
+    defaultViewport: null,
+    headless: !process.env.HEADED,
+    args: ['--no-sandbox', '--disable-gpu', '--window-size=1920,1080'],
+  });
+  page = await browser.newPage();
+});
+test('place holder', async () => {
+  await page.goto(LOCAL_URL);
+});
+afterAll(async () => {
+  await browser.close();
 });
