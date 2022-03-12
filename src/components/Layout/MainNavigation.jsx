3ccae7fe771458f2c9ea9b2d86cbe08a3f313e1e
Omer Arzi <arziomer@gmail.com>
2022-03-12 21:29:34 +0200

    Continue on styling menu and learning Bootstrap

--- src/components/Layout/MainNavigation.jsx
+++ src/components/Layout/MainNavigation.jsx
@@ -1,5 +1,8 @@
-import { NavLink } from 'react-router-dom';
-import classes from './MainNavigation.module.css';
+/* eslint-disable no-unused-vars */
+// import { NavLink } from 'react-router-dom';
+import {
+  Nav, Navbar, NavDropdown, Container,
+} from 'react-bootstrap';
 
 function MainNavigation() {
   const tabs = ['login', 'sign-up'];
@@ -10,26 +13,41 @@ function MainNavigation() {
     return myText;
   };
 
-  const mapTabs = () => tabs.map((item, index) => (
-    <li key={index}>
-      <NavLink to={`/${item}`} activeClassName={classes.active}>
-        {formatTabText(item)}
-      </NavLink>
-    </li>
-  ));
+  // const mapTabs = () => tabs.map((item, index) => (
+  //   <li key={index}>
+  //     <NavLink to={`/${item}`} activeClassName={classes.active}>
+  //       {formatTabText(item)}
+  //     </NavLink>
+  //   </li>
+  // ));
 
   return (
-    <header>
-      <nav className={classes.nav}>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="#home">
         <img
-          className={classes.logo}
           src="./photo/logo.png"
           width="350px"
           alt="Logo"
         />
-        <ul>{mapTabs()}</ul>
-      </nav>
-    </header>
+
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="/about">About</Nav.Link>
+          <NavDropdown title="System" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="/coins-info">Coins System</NavDropdown.Item>
+            <NavDropdown.Item href="/workflow">How it works</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="/rules">Rules</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Nav>
+          <Nav.Link href="/login">Login</Nav.Link>
+          <Nav.Link href="/Sign-up">Sign-up</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
