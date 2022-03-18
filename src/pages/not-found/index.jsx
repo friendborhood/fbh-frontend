52cbc98f804c788aece83822e63284600061611a
Omer Gery <omergery1996@gmail.com>
2022-03-18 23:11:55 +0200

    change site content

--- src/pages/not-found/index.jsx
+++ src/pages/not-found/index.jsx
@@ -1,7 +1,7 @@
 function NotFound() {
   return (
     <div className="centered">
-      {/* <img src="./photo/icon.jpg"></img> */}
+      <img src="./photo/icon-removebg.png" alt="404 Page not found" />
       <p>{'Oops , this page doesn\'t exist 🐿️'}</p>
     </div>
   );
