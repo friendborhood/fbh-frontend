8f5100e39622a2fd4915b66ffc43430b5ff675af
Omer Gery <omergery1996@gmail.com>
2022-04-09 19:08:26 +0300

    Update index.js

--- src/pages/additional-details/utils/index.js
+++ src/pages/additional-details/utils/index.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-param-reassign */
 import { network } from '../../../network';
 
 export const handleSubmitDetails = async (data) => {
   const { userName } = data;
+  delete data.userName;
   try {
     await network.patch(`/user/${userName}`, data);
   } catch (e) {
