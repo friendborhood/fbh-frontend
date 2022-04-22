2389f1f8aeb9e0ee9d2f6225b74c5f9a6a7dbcf9
Omer Gery <omergery1996@gmail.com>
2022-04-22 19:50:40 +0300

    globalFont -> GLOBAL_FONT

--- src/GlobalStyling.jsx
+++ src/GlobalStyling.jsx
@@ -2,4 +2,4 @@ export const GLOBAL_SCARLET = '#AA332F';
 export const GLOBAL_LIGHTGRAY = '#99A0A9';
 export const FORM_BOTTON_HEIGHT = '45px';
 export const BUTTON_RADIUS = '8px';
-export const globalFont = 'heebo';
+export const GLOBAL_FONT = 'heebo';
