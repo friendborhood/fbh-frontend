5a90a2975cc060fefcaed01e4a94bb42ca6723f7
Omer Gery <omergery1996@gmail.com>
2022-05-07 16:04:18 +0300

    Update HomePage.jsx

--- src/pages/home-page/HomePage.jsx
+++ src/pages/home-page/HomePage.jsx
@@ -1,9 +1,51 @@
-import React from 'react';
+/* eslint-disable react/button-has-type */
+import React, { useState } from 'react';
 
-function HomePage() {
+function FileUploadPage() {
+  const [selectedFile, setSelectedFile] = useState();
+  const [isSelected, setIsSelected] = useState(false);
+
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsSelected(true);
+  };
+
+  const handleSubmission = () => {
+  };
+
   return (
-    <div />
+    <div>
+      <input type="file" name="file" onChange={changeHandler} />
+      {isSelected ? (
+        <div>
+          <p>
+            Filename:
+            {' '}
+            {selectedFile.name}
+          </p>
+          <p>
+            Filetype:
+            {' '}
+            {selectedFile.type}
+          </p>
+          <p>
+            Size in bytes:
+            {' '}
+            {selectedFile.size}
+          </p>
+          <p>
+            lastModifiedDate:
+            {' '}
+            {selectedFile.lastModifiedDate.toLocaleDateString()}
+          </p>
+        </div>
+      ) : (
+        <p>Select a file to show details</p>
+      )}
+      <div>
+        <button onClick={handleSubmission}>Submit</button>
+      </div>
+    </div>
   );
 }
-
-export default HomePage;
+export default FileUploadPage;
