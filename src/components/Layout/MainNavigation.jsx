be2779b6d57aa455ef349a9a495c8fde70cd815c
Omer Gery <omergery1996@gmail.com>
2022-05-20 21:01:18 +0300

    Update MainNavigation.jsx

--- src/components/Layout/MainNavigation.jsx
+++ src/components/Layout/MainNavigation.jsx
@@ -43,6 +43,8 @@ function MainNavigation() {
           : (
             <>
               <div>{`Welcome, ${userName}`}</div>
+              <Link to={PAGES.DASHBOARD}><div className="dashboard">Dashboard</div></Link>
+              <Link to={PAGES.ADDITIONAL_DETAILS}><div className="additional-details">Your Info</div></Link>
               <div className="login" onClick={logoutHandler}>Logout</div>
             </>
           )}
