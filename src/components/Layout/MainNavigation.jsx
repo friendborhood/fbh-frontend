ec7a8ca78ccdba2667c00e0b47ec08da9cf5ebb6
Omer Arzi <arziomer@gmail.com>
2022-05-20 15:57:43 +0300

    css update

--- src/components/Layout/MainNavigation.jsx
+++ src/components/Layout/MainNavigation.jsx
@@ -42,7 +42,7 @@ function MainNavigation() {
           )
           : (
             <>
-              <div className="welcome">{`Welcome, ${userName}`}</div>
+              <div>{`Welcome, ${userName}`}</div>
               <div className="login" onClick={logoutHandler}>Logout</div>
             </>
           )}
--- src/components/Layout/main-navigation-utils/index.js
+++ src/components/Layout/main-navigation-utils/index.js
@@ -68,11 +68,10 @@ export const MainNavigationStyle = styled.div`
       color: ${GLOBAL_LIGHTGRAY};
       font-weight: 400, normal;
       font-style: 'feebo';
-
-      &.menu-item, &.login {
-        @media only screen and (max-width: ${MOBILE_STYLE.max_width}) {
+      @media only screen and (max-width: ${MOBILE_STYLE.max_width}) {
           display: none;
-        }
+      }
+      &.menu-item, &.login {
         cursor: pointer;
         &:active, :hover, :focus {
           color: ${GLOBAL_SCARLET};
@@ -81,15 +80,6 @@ export const MainNavigationStyle = styled.div`
         }
       }
     }
-
-  }
-
-  & p {
-    margin-top: 45px;
-    color: ${GLOBAL_LIGHTGRAY};
-    font-weight: 400, normal;
-    font-style: 'feebo';
-    border: solid;
   }
 
   & a {
