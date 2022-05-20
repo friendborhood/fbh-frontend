15683647eec110e451c67b411c83032fc41cad99
Omer Arzi <arziomer@gmail.com>
2022-05-20 17:56:37 +0300

    Added footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+  display: flex;
+  position: absolute;
+  flex-direction: column;
+  width: 100%;
+  height: 88px;
+  justify-content: center;
+  font-family: 'Heebo';
+  font-style: normal;
+  font-weight: 300;
+  font-size: 12px;
+  bottom: 0;
+  & div {
+      &.rights{
+        margin-left:32px;   
+      }
+  }
+`;
+
+function Footer() {
+  return (
+    <StyledFooter>
+      <div className="rights">© 2022 FriendBorHood</div>
+    </StyledFooter>
+  );
+}
+
+export default Footer;
