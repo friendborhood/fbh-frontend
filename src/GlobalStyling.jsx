13443e0f682ed8d6004749974197c17b95c206a4
Omer Arzi <arziomer@gmail.com>
2022-05-21 10:51:40 +0300

    Heebo

--- src/GlobalStyling.jsx
+++ src/GlobalStyling.jsx
@@ -2,7 +2,7 @@ export const GLOBAL_SCARLET = '#AA332F';
 export const GLOBAL_LIGHTGRAY = '#99A0A9';
 export const FORM_BOTTON_HEIGHT = '45px';
 export const BUTTON_RADIUS = '8px';
-export const GLOBAL_FONT = 'heebo';
+export const GLOBAL_FONT = 'Heebo';
 export const LOADER_PARAMS = {
   height: 40,
   width: 30,
