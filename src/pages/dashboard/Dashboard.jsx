ae08b5a48627020f86b4dc63737d44948356a50c
Omer Arzi <arziomer@gmail.com>
2022-06-24 00:36:08 +0300

    Edited dashboard page
    - made colored background.
    - addition: updraded the footer that it won't get in the user's way.

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,40 +1,54 @@
-import React, { useState } from 'react';
-import BoxInput from '../../components/BoxInput';
-import { END_POINTS, network } from '../../network';
+// import React, { useState } from 'react';
+import styled from 'styled-components';
+// import BoxInput from '../../components/BoxInput';
+// import { END_POINTS, network } from '../../network';
+import { SECONDARY_BACKGROUND } from '../../GlobalStyling';
+
+const DashboardPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+  border: solid;
+  background-color: ${SECONDARY_BACKGROUND};
+  opacity: 11%;
+`;
 
 function Dashboard() {
-  const [radius, setRadius] = useState(5000);
-  const [offers, setOffers] = useState([]);
+  // const [radius, setRadius] = useState(5000);
+  // const [offers, setOffers] = useState([]);
   return (
-    <form>
-      <BoxInput
-        label="Select radius"
-        id="radius"
-        state={radius}
-        setState={setRadius}
-        placeHolder="radius"
-      />
-      <button
-        type="button"
-        onClick={async () => {
-          const { data: fetchedOffers } = await network.get(`${END_POINTS.OFFERS}/in-area`, { params: { radius } });
-          setOffers(fetchedOffers);
-        }}
-      >
+    <DashboardPage />
+    // <form>
+    //   <BoxInput
+    //     label="Select radius"
+    //     id="radius"
+    //     state={radius}
+    //     setState={setRadius}
+    //     placeHolder="radius"
+    //   />
+    //   <button
+    //     type="button"
+    //     onClick={async () => {
+    //       const { data: fetchedOffers } = await network.get(`${END_POINTS.OFFERS}/in-area`,
+    // { params: { radius } });
+    //       setOffers(fetchedOffers);
+    //     }}
+    //   >
 
-        Click me for offers
+  //     Click me for offers
 
-      </button>
-      <div>
-        {' '}
-        there are
-        {' '}
-        {offers.length}
-        {' '}
-        offers , all offers:
-        {JSON.stringify(offers)}
-      </div>
-    </form>
+  //   </button>
+  //   <div>
+  //     {' '}
+  //     there are
+  //     {' '}
+  //     {offers.length}
+  //     {' '}
+  //     offers , all offers:
+  //     {JSON.stringify(offers)}
+  //   </div>
+  // </form>
   );
 }
 
