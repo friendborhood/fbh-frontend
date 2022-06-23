4e37a236c77fbbf12b06e10334cb7f225d9eb78c
Omer Arzi <arziomer@gmail.com>
2022-06-24 00:46:50 +0300

    fixed background

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -5,36 +5,40 @@ import styled from 'styled-components';
 import { SECONDARY_BACKGROUND } from '../../GlobalStyling';
 
 const DashboardPage = styled.div`
+  position: absolute;
   display: flex;
   flex-direction: column;
-  height: 100%;
   width: 100%;
-  border: solid;
+  height: 100%;  
   background-color: ${SECONDARY_BACKGROUND};
   opacity: 11%;
+  border: solid;
 `;
 
 function Dashboard() {
   // const [radius, setRadius] = useState(5000);
   // const [offers, setOffers] = useState([]);
   return (
-    <DashboardPage />
-    // <form>
-    //   <BoxInput
-    //     label="Select radius"
-    //     id="radius"
-    //     state={radius}
-    //     setState={setRadius}
-    //     placeHolder="radius"
-    //   />
-    //   <button
-    //     type="button"
-    //     onClick={async () => {
-    //       const { data: fetchedOffers } = await network.get(`${END_POINTS.OFFERS}/in-area`,
-    // { params: { radius } });
-    //       setOffers(fetchedOffers);
-    //     }}
-    //   >
+    <DashboardPage>
+      <div />
+    </DashboardPage>
+
+  // <form>
+  //   <BoxInput
+  //     label="Select radius"
+  //     id="radius"
+  //     state={radius}
+  //     setState={setRadius}
+  //     placeHolder="radius"
+  //   />
+  //   <button
+  //     type="button"
+  //     onClick={async () => {
+  //       const { data: fetchedOffers } = await network.get(`${END_POINTS.OFFERS}/in-area`,
+  // { params: { radius } });
+  //       setOffers(fetchedOffers);
+  //     }}
+  //   >
 
   //     Click me for offers
 
