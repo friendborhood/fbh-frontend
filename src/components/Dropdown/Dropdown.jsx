978e78184744b9d078b5712b875645082fd2c03a
Omer Arzi <arziomer@gmail.com>
2022-06-24 13:51:24 +0300

    seperated style of dropdown

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,85 +1,9 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/button-has-type */
 import { useState } from 'react';
-import styled from 'styled-components';
-import { GLOBAL_FONT, GLOBAL_SCARLET } from '../../GlobalStyling';
+import { CustomDropdown } from './style';
 import dropdownarrow from '../../images/dropdown-arrow.svg';
 
-const CustomDropdown = styled.div`
-  & button {
-      display: flex;
-      position: relative;
-      flex-direction: row;
-      align-items: center;
-      padding: 8px 16px;
-      gap: 8px;
-      min-width: 163px;
-      height: 45px;
-      background: ${GLOBAL_SCARLET};
-      font-family: ${GLOBAL_FONT};
-      color: white;
-      border-radius: 70px;
-      font-style: normal;
-      font-weight: 400;
-      font-size: 20px;
-      line-height: 29px;
-
-      & img {
-        &.open{
-          transform : rotate(180deg);
-          transition: transform 300ms ease;
-        }
-        &.close {
-          transform : rotate(0deg);
-          transition: transform 300ms ease;
-        }
-      }
-  }
-  & ul {
-    &.dropdown-content {
-      position: relative;
-      top: 100%;
-      width: 100%;
-      perspective: 1000px;
-      display: none;
-      background-color: white;
-      min-width: 160px;
-      box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-      border-radius: 70px; 
-      margin-top: 10px;
-    }
-    &.dropdown_content--animated {
-      display: block;
-    }
-    &.dropdown_menu-6{
-      animation: ${(props) => (props.isOpen ? 'growDown 300ms ease-in-out forwards' : 'growUp 200ms ease-out forwards')};
-      transform-origin: top center;
-    }
-
-    @keyframes growDown {
-      0% {
-        transform: scaleY(0)
-      }
-      80% {
-        transform: scaleY(0.7)
-      }
-      100% {
-        transform: scaleY(1)
-      }
-    }
-    @keyframes growUp {
-      0% {
-        transform: scaleY(1)
-      }
-      80% {
-        transform: scaleY(1.1)
-      }
-      100% {
-        transform: scaleY(0)
-      }
-    }
-  }
-`;
 export function Dropdown({ options, chosen = '', setChosen }) {
   const [isOpen, setIsOpen] = useState(false);
 
