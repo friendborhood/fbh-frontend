c96ff6e87ddd48496e1636e1448eb65b3d036bc8
Omer Arzi <arziomer@gmail.com>
2022-06-24 20:38:38 +0300

    Update DashboardWrapper.jsx

--- src/components/DashboardWrapper/DashboardWrapper.jsx
+++ src/components/DashboardWrapper/DashboardWrapper.jsx
@@ -6,7 +6,13 @@ const StyledDashboardWrapper = styled.main`
   position: absolute;
   width: 100%;
   height: 100%;
+  animation: colorize 0.4s linear;
   background-color: ${SECONDARY_BACKGROUND};
+
+  @keyframes colorize {
+    0% {background-color: inherit;}
+    100% {background-color: ${SECONDARY_BACKGROUND};}
+  }
 `;
 
 function DashboardWrapper({ children }) {
