528434aac60b0e4b99ea902bbe83759318d984cc
Omer Arzi <arziomer@gmail.com>
2022-06-24 12:42:19 +0300

    Update Dropdown.jsx

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/button-has-type */
 import { useState } from 'react';
 import styled from 'styled-components';
@@ -7,6 +8,7 @@ import dropdownarrow from '../../images/dropdown-arrow.svg';
 const CustomDropdown = styled.div`
   & button {
       display: flex;
+      position: relative;
       flex-direction: row;
       align-items: center;
       padding: 8px 16px;
@@ -33,21 +35,35 @@ const CustomDropdown = styled.div`
         }
       }
   }
+  & div {
+    &.dropdown-content {
+      position: relative;
+        display: ${(props) => (props.isOpen ? 'flex' : 'flex')};
+        height: ${(props) => (props.isOpen ? '100px' : '0')};
+        transition: height 0.2s ease;
+        margin-top: 10px;
+        background-color: white;
+        min-width: 160px;
+        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    }
+  }
 `;
 export function Dropdown({ chosen = '', setChosen }) {
   const [isOpen, setIsOpen] = useState(false);
-  console.log(chosen, setChosen);
 
   const onClickHandler = async () => {
     setIsOpen(!isOpen);
   };
+
   return (
-    <CustomDropdown>
-      <button className="dropbtn" onClick={onClickHandler} isOpen={isOpen}>
+    <CustomDropdown isOpen={isOpen}>
+      <button className="dropbtn" onClick={onClickHandler}>
         <div>{chosen}</div>
         <img src={dropdownarrow} alt="arrow" className={isOpen ? 'open' : 'close'} />
       </button>
-      <div className="dropdown-content" />
+      <div className="dropdown-content">
+        <div />
+      </div>
     </CustomDropdown>
   );
 }
