7b24b05e5e42fec54a61a4f07519905c3b14c2ea
Omer Arzi <76008630+OmerArzi@users.noreply.github.com>
2022-08-21 13:05:48 +0300

    Add offer (#43)
    * start add item panel
    
    * Upload item updates
    
    * Stages
    
    Stage 1 and some of stage 2
    
    * fix offer image show up after upload
    
    * fix upload and remove 'image' state
    
    * upload offer
    
    Co-authored-by: OmerArzi <omerA@moveoboost.com>
    Co-authored-by: Omer Gery <omergery1996@gmail.com>

--- src/pages/upload-offer/uploadOffer.jsx
+++ src/pages/upload-offer/uploadOffer.jsx
@@ -1,17 +1,26 @@
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/style-prop-object */
 /* eslint-disable no-shadow */
 /* eslint-disable react/button-has-type */
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import { ProgressBar, Step } from 'react-step-progress-bar';
 import BoxInput from '../../components/BoxInput';
 import DropDown from '../../components/drop-down';
 import { END_POINTS, fetchUserData, network } from '../../network';
 import { displayMessage } from '../../utils/handle-device-middleware';
+import 'react-step-progress-bar/styles.css';
+import { GLOBAL_SCARLET } from '../../GlobalStyling';
+import { UploadOfferStyle } from './uploadOfferStyle';
+import miniIcon from '../../images/mini-icon-removebg.png';
+import uploadButton from '../../images/upload-img-button.svg';
+import 'animate.css';
 
 function UploadOffer() {
   const CLOUD_NAME = 'dxjhkogtp';
   const PRESET = 'tc7nz7hr';
   const [condition, setCondition] = useState('');
-  const [image, setImage] = useState('');
   const [item, setItem] = useState('');
   const [itemNames, setItemNames] = useState(['Driller']);
   const [description, setDescription] = useState('');
@@ -19,7 +28,11 @@ function UploadOffer() {
   const [cloudinaryUrl, setCloudinaryUrl] = useState('');
   const [itemsMap, setItemsMap] = useState({});
   const [userLocation, setUserLocation] = useState({});
-
+  const [stepOne, setStepOne] = useState(true);
+  const [stepTwo, setStepTwo] = useState(false);
+  const [stepThree, setStepThree] = useState(false);
+  const [progressPrecent, setProgressPrecent] = useState(0);
+  const [currentStep, setCurrentStep] = useState(1);
   const swapKeysAndValues = (obj) => {
     const swapped = Object.entries(obj).map(
       ([key, value]) => [value.itemName, key],
@@ -27,6 +40,7 @@ function UploadOffer() {
 
     return Object.fromEntries(swapped);
   };
+
   const fetchItems = async () => {
     const { data: currentItems } = await network.get(END_POINTS.ITEM);
     const itemsNamesFormatted = Object.values(currentItems).map((item) => item.itemName);
@@ -51,24 +65,144 @@ function UploadOffer() {
   };
   const uploadOffer = async () => {
     try {
-      await uploadToCloudinary(image);
-      await network.post(`${END_POINTS.OFFERS}`, {
-        imageUrl: cloudinaryUrl,
-        itemId: itemsMap[item],
-        priceAsked: price,
-        description,
-        condition,
-        state: 'Available',
-        location: userLocation,
-      });
-      displayMessage('Offer uploaded successfully');
+      if (cloudinaryUrl && price && condition) {
+        await network.post(`${END_POINTS.OFFERS}`, {
+          imageUrl: cloudinaryUrl,
+          itemId: itemsMap[item],
+          priceAsked: price,
+          description,
+          condition,
+          state: 'Available',
+          location: userLocation,
+        });
+        displayMessage('Offer uploaded successfully');
+      } else {
+        console.warn('No image uploaded');
+        displayMessage('Please fill all the required fields');
+      }
     } catch (e) {
       displayMessage(`Error uploading offer ${JSON.stringify(e.response.data)}`);
     }
   };
+
+  const onDescriptionChangeHandler = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const clickHandler = () => {
+    if (currentStep === 1) {
+      setStepOne(false);
+      setStepTwo(true);
+      setCurrentStep(2);
+      setProgressPrecent(50);
+    } else if (currentStep === 2) {
+      setStepTwo(false);
+      setStepThree(true);
+      setCurrentStep(3);
+      setProgressPrecent(100);
+    } else if (currentStep === 3) {
+      setStepThree(false);
+      setCurrentStep(4);
+    }
+  };
+
   return (
-    <div>
-      <h1>Upload an offer</h1>
+    <UploadOfferStyle>
+      <div className="progress-bar-container">
+        <ProgressBar
+          percent={progressPrecent}
+          width="876px"
+          filledBackground={`linear-gradient(to right, ${GLOBAL_SCARLET}, ${GLOBAL_SCARLET}`}
+        >
+          <Step transition="scale">
+            {({ accomplished }) => (
+              <img
+                style={{ filter: `grayscale(${accomplished ? 0 : 80}%)` }}
+                width="35"
+                src={miniIcon}
+                alt=""
+              />
+            )}
+          </Step>
+          <Step transition="scale">
+            {({ accomplished }) => (
+              <img
+                style={{ filter: `grayscale(${accomplished ? 0 : 30}%)` }}
+                width="35"
+                src={miniIcon}
+                alt=""
+              />
+            )}
+          </Step>
+          <Step transition="scale">
+            {({ accomplished }) => (
+              <img
+                style={{ filter: `grayscale(${accomplished ? 0 : 30}%)` }}
+                width="35"
+                src={miniIcon}
+                alt=""
+              />
+            )}
+          </Step>
+        </ProgressBar>
+      </div>
+      <div className="main-panel">
+        {/* Step One: */}
+        <div className={`single-step ${stepOne === true ? 'displayOnStart' : 'displayOff'}`}>
+          <DropDown className="item-selection on-top" options={itemNames} setState={setItem} state={item} />
+          <BoxInput
+            borderWidth="1px"
+            label="Condition"
+            id="Condtion"
+            state={condition}
+            placeHolder="Condition"
+            setState={setCondition}
+          />
+          <div className="description-container">
+            <div className="field-title">About the item</div>
+            <textarea
+              className="description-input"
+              id="description"
+              onChange={onDescriptionChangeHandler}
+              placeholder="  Describe your item"
+            />
+          </div>
+        </div>
+        {/* Step Two: */}
+        <div className={`single-step ${stepTwo === true ? 'displayOn' : 'displayOff'}`}>
+          <div className="single-field">
+            <div className="field-title">Upload an image</div>
+            <label htmlFor="image-upload">
+              <img src={uploadButton} alt="" />
+              <input
+                type="file"
+                id="image-upload"
+                onChange={(e) => {
+                  const uploadedImage = e.target.files[0];
+                  if (uploadedImage) {
+                    uploadToCloudinary(uploadedImage);
+                  } else {
+                    console.warn('no image on upload event');
+                  }
+                }}
+              />
+            </label>
+          </div>
+          <BoxInput
+            borderWidth="1px"
+            label="Item's price"
+            id="price"
+            state={price}
+            placeHolder="Price"
+            setState={setPrice}
+          />
+        </div>
+        {/* "Next Step" Button: */}
+        <button className="step" onClick={stepTwo ? uploadOffer : clickHandler}>{stepTwo ? 'Submit Offer' : 'Next Step'}</button>
+        <img hidden={!cloudinaryUrl} style={{ maxHeight: 300, maxWidth: 300 }} alt="offer" src={cloudinaryUrl} />
+
+      </div>
+      {/* h1>Upload an offer</h1>
       <div>
         <h4>Select an item</h4>
         <DropDown options={itemNames} setState={setItem} state={item} />
@@ -100,9 +234,8 @@ function UploadOffer() {
       </div>
       <div>
 
-        <img hidden={!image} alt="offer" src={image} />
-      </div>
-    </div>
+      </div> */}
+    </UploadOfferStyle>
   );
 }
 export default UploadOffer;
---
+++ src/pages/upload-offer/uploadOfferStyle.jsx
@@ -0,0 +1,179 @@
+import styled from 'styled-components';
+import { GLOBAL_FONT, GLOBAL_SCARLET, SECONDARY_BACKGROUND } from '../../GlobalStyling';
+
+export const UploadOfferStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding-top: 6%;
+    /* justify-items: center; */
+    align-items: center;
+    /* align-content: center; */
+    position: relative;
+    background-color: ${SECONDARY_BACKGROUND};
+    width: 100vw;
+    height: 100%;
+
+    & div.single-field {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+    }
+
+    & div.single-step {
+        display: none;
+        position: absolute;
+        gap: 20px;
+    }
+
+    & div.displayOnStart {
+        display: flex;
+        flex-direction: column;
+        animation-name: fadeInLeft;
+        animation-duration: 0.5s;
+        animation-timing-function: ease-in;
+        animation-delay: 0s;
+        animation-direction: alternate;
+        animation-iteration-count: 1;
+        animation-fill-mode: forwards;
+    }
+
+    & div.displayOn {
+        display: flex;
+        opacity: 0;
+        flex-direction: column;
+        animation-name: fadeInLeft;
+        animation-duration: 0.5s;
+        animation-timing-function: linear;
+        animation-delay: 0.2s;
+        animation-direction: alternate;
+        animation-iteration-count: 1;
+        animation-fill-mode: forwards;
+    }
+
+    & div.displayOff {
+        display: flex;
+        opacity: 1;
+        flex-direction: column;
+        animation-name: fadeOutRight;
+        animation-duration: 0.4s;
+        animation-timing-function: linear;
+        animation-delay: 0s;
+        animation-direction: alternate;
+        animation-iteration-count: 1;
+        animation-fill-mode: forwards;
+    }
+
+    & div.progress-bar-container {
+        margin-bottom: 40px;
+    }
+
+    & div { 
+        &.main-panel {
+            position: relative;
+            height: 636px;
+            width: 1375px;
+            max-width: 80%;
+            max-height: 60%;
+            background-color: white;
+            border-radius: 10px;
+            padding: 100px 75px 100px 200px;
+            box-shadow: 0px 0px 10px 0px #14171C1A;
+
+            & div.field-title {
+                color: #14171C;
+                text-align: left;
+                font-size: 17px;
+                line-height: 29px;
+                font-family: ${GLOBAL_FONT};
+                font-weight: 500;
+            }
+
+            & div.below {
+                margin-top: 20px;
+            }
+
+            & label {
+                cursor: pointer;
+                & button {
+                    display: flex;
+                    width: 80px;
+                    height: 80px;
+                    justify-content: center;
+                    align-items: center;
+                    font-size: 30px;
+                    border: solid red;
+                }
+            }
+
+            & #image-upload {
+                display: none;
+            }
+
+            & div.description-container {
+                margin-top: 20px;
+
+                & textarea.description-input {
+                    text-align: left;
+                    display: flex;
+                    padding-top: 0;
+                    flex-direction: flex-start;
+                    width: 446px;
+                    height: 114px;
+                    border-radius: 8px;
+                    border: solid 1px #99A0A9;
+                }
+
+                & textarea.description-input:focus {
+                    outline: none;
+                    border: solid 1px ${GLOBAL_SCARLET};
+                    box-shadow: none;
+                }
+            }
+
+            & button.step {
+                position: absolute;
+                border-radius: 8px;
+                width: 137px;
+                height: 45px;
+                padding: 8px;
+                background-color: ${GLOBAL_SCARLET};
+                color: white;
+                font-size: 20px;
+                line-height: 29px;
+                font-family: ${GLOBAL_FONT};
+                font-weight: 500;
+                right: 71px;
+                bottom: 70px;
+            }
+
+            & button {
+
+            }
+        }
+    }
+    @keyframes fadeInLeft {
+        0% {
+            display:flex;
+            opacity: 0;
+            transform: translateX(-40px);
+        }
+        100% {
+            display:flex;
+            opacity: 1;
+            transform: translateX(0);
+        }
+    }   
+
+    @keyframes fadeOutRight {
+        0% {
+            display:flex;
+            opacity: 1;
+            transform: translateX(0);
+        }
+        100% {
+            display: none;
+            opacity: 0;
+            transform: translateX(60px);
+        }
+    }   
+`;
