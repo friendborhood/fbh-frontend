3e62858155dfeb108f9cbaa7d02996f861a730d2
Omer Gery <omergery1996@gmail.com>
2022-09-05 21:59:18 +0300

    Update ItemCard.jsx

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -50,66 +50,67 @@ function ItemCard({ myOffers, offerData }) {
   } = offererUserData;
   const distance = (distanceFromUser / 1000).toFixed(2);
   return (
-    updatedState === 'Deleted' ? <div /> : (
-      <ReactCardFlip isFlipped={flipped} flipDirection="horizontal">
-        <Card disabled={updatedState === 'Disabled' && true} onClick={() => setFlipped(!flipped)}>
-          <img
-            src={imageUrl || fallBackImage}
-            alt="item"
-            className="item"
-          />
-          <div className="item-details">
-            <div className="info-line">
-              <div className="text large">{itemName}</div>
-              {(!myOffers && <div className="text small">{`${distance} km`}</div>)}
-              <div className="text small">{updatedState}</div>
-            </div>
-            <div className="info-line">
-              <div className="text user-info">
-                <img className="user-icon" src={offererUserIconUrl} alt="" />
-                <div className="text">{`${firstName} ${lastName}`}</div>
+    updatedState === 'Deleted' ? null
+      : (
+        <ReactCardFlip isFlipped={flipped} flipDirection="horizontal">
+          <Card disabled={updatedState === 'Disabled' && true} onClick={() => setFlipped(!flipped)}>
+            <img
+              src={imageUrl || fallBackImage}
+              alt="item"
+              className="item"
+            />
+            <div className="item-details">
+              <div className="info-line">
+                <div className="text large">{itemName}</div>
+                {(!myOffers && <div className="text small">{`${distance} km`}</div>)}
+                <div className="text small">{updatedState}</div>
               </div>
-              <div className="text small">{`${price}₪/hour`}</div>
-            </div>
-          </div>
-        </Card>
-        <Card onClick={() => setFlipped(!flipped)}>
-          {!myOffers ? (
-            <h4>
-              {' '}
-              {`Contact ${firstName}: \n ${phoneNumber ? `\n phone number is ${phoneNumber}` : ''} ${email ? `\n email is ${email}` : ''}`}
-            </h4>
-          ) : (
-            <>
-              <br />
-              <br />
-              <br />
-              <button type="button" onClick={() => handleDeleteOffer(id)}>Delete Offer</button>
-              <br />
-              <br />
-              <br />
-              <br />
-              <button type="button" onClick={() => patchOffer(id, updatedState === 'Disabled' ? 'Available' : 'Disabled')}>{`${updatedState === 'Disabled' ? 'Enable' : 'Disable'} Offer`}</button>
-            </>
-          )}
-          {!myOffers && (
-          <div className="item-details">
-            <div className="info-line">
-              <div className="text large">{itemName}</div>
-              <div className="text large">{`${distance} km`}</div>
-            </div>
-            <div className="info-line">
-              <div className="text user-info">
-                <img className="user-icon" src={offererUserIconUrl} alt="" />
-                <div className="medium">{`${firstName} ${lastName}`}</div>
+              <div className="info-line">
+                <div className="text user-info">
+                  <img className="user-icon" src={offererUserIconUrl} alt="" />
+                  <div className="text">{`${firstName} ${lastName}`}</div>
+                </div>
+                <div className="text small">{`${price}₪/hour`}</div>
               </div>
-              <div className="text large">{`${price}₪/hour`}</div>
             </div>
-          </div>
-          )}
-        </Card>
-      </ReactCardFlip>
-    ));
+          </Card>
+          <Card onClick={() => setFlipped(!flipped)}>
+            {!myOffers ? (
+              <h4>
+                {' '}
+                {`Contact ${firstName}: \n ${phoneNumber ? `\n phone number is ${phoneNumber}` : ''} ${email ? `\n email is ${email}` : ''}`}
+              </h4>
+            ) : (
+              <>
+                <br />
+                <br />
+                <br />
+                <button type="button" onClick={() => handleDeleteOffer(id)}>Delete Offer</button>
+                <br />
+                <br />
+                <br />
+                <br />
+                <button type="button" onClick={() => patchOffer(id, updatedState === 'Disabled' ? 'Available' : 'Disabled')}>{`${updatedState === 'Disabled' ? 'Enable' : 'Disable'} Offer`}</button>
+              </>
+            )}
+            {!myOffers && (
+            <div className="item-details">
+              <div className="info-line">
+                <div className="text large">{itemName}</div>
+                <div className="text large">{`${distance} km`}</div>
+              </div>
+              <div className="info-line">
+                <div className="text user-info">
+                  <img className="user-icon" src={offererUserIconUrl} alt="" />
+                  <div className="medium">{`${firstName} ${lastName}`}</div>
+                </div>
+                <div className="text large">{`${price}₪/hour`}</div>
+              </div>
+            </div>
+            )}
+          </Card>
+        </ReactCardFlip>
+      ));
 }
 
 export default ItemCard;
